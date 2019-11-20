b6a33c622812b68f4a1fe1a6c57be3005882cbd7
Anton Johansson <johaant@net.chalmers.se>
2019-11-20 08:41:42 +0100

    Add: Added the main files

---
+++ generate_data.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 08:23:48 2019
+
+@author: johaant
+"""
+
+##
+import numpy as np
+
+def generate_data_points(num_data_train, num_features, plot_points = False):
+    
+    #num_data_train = 500
+    #num_features = 2
+    x_data = np.random.normal(0,10,(num_data_train, num_features))
+    theta = np.random.normal(0,1,num_features + 1)
+    
+    
+    def logistic(x):
+      return 1 / (1 + np.exp(-x))
+    
+    def generate_y(x, theta):
+      y_prob = logistic(np.sum(x_data * theta[:-1], axis = 1) + theta[-1])
+      class_vector = np.random.binomial(1,y_prob)
+      return class_vector
+    
+    def get_predictions(x, theta):
+    
+      y_prob = logistic(np.sum(x_data * theta[:-1], axis = 1) + theta[-1])
+      one_class = np.where(y_prob > 0.5)
+      class_vec = np.zeros(num_data_train)
+      class_vec[one_class] = 1
+      y_data = class_vec
+      return y_data
+    
+    
+    y_data = generate_y(x_data, theta)
+    
+    if plot_points == True:
+        x_plot = np.linspace(-3,3,100)
+        y_plot = -theta[-1]/theta[1] - theta[0]/theta[1]*x_plot
+        import matplotlib.pyplot as plt
+        plt.scatter(x_data[y_data == 0,0],x_data[y_data == 0,1], c = 'r')
+        plt.scatter(x_data[y_data == 1,0],x_data[y_data == 1,1], c = 'b')
+        plt.plot(x_plot, y_plot)
+    return (x_data, y_data)
---
+++ mainFile.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 08:21:35 2019
+
+@author: johaant
+"""
+
+from generate_data import generate_data_points
+
+from autograd import grad
+
+num_data_train = 500
+num_features = 2
+x_data, y_data = generate_data_points(num_data_train, num_features)
+
+
+
+
+
+
+
+
+
+
+
+
